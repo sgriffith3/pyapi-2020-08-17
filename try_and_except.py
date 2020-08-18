e520fd940a5334ec88841c583158ab7e584d17ad
Sam <sgriffith@alta3.com>
2020-08-18 21:59:08 +0000

    Tuesday afternoon content

---
+++ try_and_except.py
@@ -0,0 +1,55 @@
+x = 11
+
+y = 0
+
+print(x + y)
+
+print(x - y)
+
+#print(x / y)
+
+#print("You can't divide by Zero")
+
+
+pi = "Raspberry"
+#pi = 3.14
+print(pi)
+print(type(pi))    
+try:
+    print(x / y)
+    print(int(pi))
+    print("Yay!")
+except ValueError:
+    print("Boo!")
+except ZeroDivisionError:
+    print("Zero cannot be divided by")
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
