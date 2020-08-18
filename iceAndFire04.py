2ecab6b74d88089a140e9609f7f7a68e5eae927b
Sam <sgriffith@alta3.com>
2020-08-18 19:00:29 +0000

    after lunch Tuesday work

---
+++ iceAndFire04.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Alta3 Research - Exploring OpenAPIs with requests"""
+# documentation for this API is at
+# https://anapioficeandfire.com/Documentation
+
+import requests
+import pprint
+
+AOIF_CHAR = "https://www.anapioficeandfire.com/api/characters"
+
+def main():
+    ## Ask user for input
+    got_charToLookup = input("What is the name of the character we should lookup? " )
+
+    ## Send HTTPS GET to the API of ICE and Fire character resource
+    gotresp = requests.get(AOIF_CHAR + "?name=" + got_charToLookup)
+
+    ## Decode the response
+    got_dj = gotresp.json()
+
+    pprint.pprint(got_dj)
+    print(f"The character {got_charToLookup} has the URL: {got_dj[0]['url']}")
+    print(f"The character {got_charToLookup} has the allegiances: {got_dj[0]['allegiances']}")
+    print(f"The character {got_charToLookup} is in the books:")
+    for book in got_dj[0]['books']:
+        print(f"- {book}")
+
+if __name__ == "__main__":
+    main()
+
